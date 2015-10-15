9ae506c17831ddae24c75cba2c9995f5993a1373
asmadmin <sgourmelon@asmolding.com>
2015-10-15 12:27:47 +0200

    doctrine migrations

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Asmolding\Bundle\AsmoldingBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
